ad777bbdd7692346fefd64a7cc68bf5b2c1bb45d
Shivam Sharma <sdsharma@ucsd.edu>
2017-10-28 15:38:56 -0700

    Add some modals

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
-import { Component, AfterViewInit } from '@angular/core';
+import { Component, AfterViewInit, ViewContainerRef } from '@angular/core';
 import { DomSanitizer } from '@angular/platform-browser';
 import { MatIconRegistry } from '@angular/material';
 import { TdMediaService } from '@covalent/core';
+import { TdDialogService } from '@covalent/core';
 
 @Component({
   selector: 'app-root',
@@ -15,7 +16,9 @@ export class AppComponent implements AfterViewInit {
 
   constructor(public media: TdMediaService,
               private _iconRegistry: MatIconRegistry,
-              private _domSanitizer: DomSanitizer) {
+              private _domSanitizer: DomSanitizer,
+              private _dialogService: TdDialogService,
+              private _viewContainerRef: ViewContainerRef) {
                 
               this._iconRegistry.addSvgIconInNamespace('assets', 'covalent',
               this._domSanitizer.bypassSecurityTrustResourceUrl('https://raw.githubusercontent.com/Teradata/covalent-quickstart/develop/src/assets/icons/covalent.svg'));
@@ -24,5 +27,60 @@ export class AppComponent implements AfterViewInit {
 
   ngAfterViewInit(): void {
       this.media.broadcast();
+  }
+
+  handleNewMessage(): void {
+  	document.getElementById('messageInput').getElementsByTagName('input')[0].focus();
+  }
+
+  openSettings(): void {
+    this._dialogService.openConfirm({
+      message: 'Edit services connected and logout from here',
+      disableClose: false,
+      viewContainerRef: this._viewContainerRef,
+      title: 'Settings',
+      cancelButton: 'Save',
+      acceptButton: 'Logout'
+    }).afterClosed().subscribe((accept: boolean) => {
+      if (accept) {
+        // DO SOMETHING
+      } else {
+        // DO SOMETHING ELSE
+      }
+    });
+  }
+
+  openPriorities(): void {
+  	this._dialogService.openConfirm({
+      message: 'Add priorities from here',
+      disableClose: false,
+      viewContainerRef: this._viewContainerRef,
+      title: 'Add Priority',
+      cancelButton: 'Canel',
+      acceptButton: 'Save'
+    }).afterClosed().subscribe((accept: boolean) => {
+      if (accept) {
+        // DO SOMETHING
+      } else {
+        // DO SOMETHING ELSE
+      }
+    });
+  }
+
+  modifyPriorities(): void {
+  	this._dialogService.openConfirm({
+      message: 'Modify priorities from here',
+      disableClose: false,
+      viewContainerRef: this._viewContainerRef,
+      title: 'Modify Priority',
+      cancelButton: 'Canel',
+      acceptButton: 'Save'
+    }).afterClosed().subscribe((accept: boolean) => {
+      if (accept) {
+        // DO SOMETHING
+      } else {
+        // DO SOMETHING ELSE
+      }
+    });
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,10 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatCardModule, MatButtonModule, MatMenuModule, MatInputModule, MatListModule, MatIconModule, MatSidenavModule, MatToolbarModule, MatTooltipModule, MatDialogModule } from '@angular/material';
+import { MatCardModule, MatButtonModule, MatMenuModule, MatInputModule, MatListModule, MatIconModule, MatSidenavModule, MatToolbarModule, MatTooltipModule, MatDialogModule, MatCheckboxModule } from '@angular/material';
 import { AppComponent } from './app.component';
-import { CovalentLayoutModule, CovalentStepsModule, CovalentSearchModule, CovalentMenuModule, CovalentMediaModule, CovalentNotificationsModule, CovalentFileModule } from '@covalent/core';
+import { CovalentLayoutModule, CovalentStepsModule, CovalentSearchModule, CovalentMenuModule, CovalentMediaModule, CovalentNotificationsModule, CovalentFileModule, CovalentDialogsModule } from '@covalent/core';
 import { EmojiModule } from 'angular2-emoji';
-
-
 import 'hammerjs';
 
 @NgModule({
@@ -17,13 +15,14 @@ import 'hammerjs';
   ],
   imports: [
     BrowserModule,
-    MatCardModule, MatButtonModule, MatMenuModule, MatInputModule, MatListModule, MatIconModule, MatSidenavModule, MatToolbarModule, MatTooltipModule, MatDialogModule,
+    MatCardModule, MatButtonModule, MatMenuModule, MatInputModule, MatListModule, MatIconModule, MatSidenavModule, MatToolbarModule, MatTooltipModule, MatDialogModule, MatCheckboxModule,
     BrowserAnimationsModule,
     FormsModule,
     HttpModule,
     CovalentLayoutModule,
     CovalentStepsModule,
     CovalentSearchModule,
+    CovalentDialogsModule,
     CovalentMenuModule,
     CovalentMediaModule,
     CovalentNotificationsModule,
